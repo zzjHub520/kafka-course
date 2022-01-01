297b4c431c028170faa1d3bcec07d1c38b27ec95
zzj <912290893@qq.com>
2022-01-02 00:51:25 +0800

    asdf

---
+++ 千锋教育最新kafka入门到精通教程kafka快速入门，阿里P7架构师带你深度解析/code/kafka-springboot-demo/src/main/java/com/example/kafkaspringbootdemo/controller/MyKafkaController.java
@@ -0,0 +1,4 @@
+package com.example.kafkaspringbootdemo.controller;
+
+public class MyKafkaController {
+}
---
+++ 千锋教育最新kafka入门到精通教程kafka快速入门，阿里P7架构师带你深度解析/code/kafka-springboot-demo/src/test/java/com/example/kafkaspringbootdemo/KafkaSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
